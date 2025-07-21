f1179e02b17b08c65dd46c6d6a86fc38ac91f63c
Navindu Premaratne <navindunimnal@gmail.com>
2025-07-22 00:48:40 +0530

    first commit

---
+++ FatigueDetectorWatchApp Watch App/FatigueDetectorWatchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FatigueDetectorWatchAppApp.swift
+//  FatigueDetectorWatchApp Watch App
+//
+//  Created by Navindu Premaratne on 2025-07-21.
+//
+
+import SwiftUI
+
+@main
+struct FatigueDetectorWatchApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FatigueDetectorWatchApp Watch AppTests/FatigueDetectorWatchApp_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  FatigueDetectorWatchApp_Watch_AppTests.swift
+//  FatigueDetectorWatchApp Watch AppTests
+//
+//  Created by Navindu Premaratne on 2025-07-21.
+//
+
+import Testing
+@testable import FatigueDetectorWatchApp_Watch_App
+
+struct FatigueDetectorWatchApp_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
